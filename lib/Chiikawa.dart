ca78fbf80d77fd2d86eee714a7ba0cf0f371a75e
ouoxii <yuchuner317@gmail.com>
2024-03-30 22:16:21 +0800

    add properties

--- lib/Chiikawa.dart
+++ lib/Chiikawa.dart
@@ -2,12 +2,18 @@ class Chiikawa {
   final String name;
   final String image;
   final String text;
-  const Chiikawa({required this.name, required this.image, required this.text});
+  final String birthday;
+  const Chiikawa(
+      {required this.name,
+      required this.image,
+      required this.text,
+      required this.birthday});
   factory Chiikawa.fromJson(Map<String, dynamic> json) {
     return Chiikawa(
       name: json['name'],
       image: json['Image'],
       text: json['description'],
+      birthday: json['birthday'],
     );
   }
 }
